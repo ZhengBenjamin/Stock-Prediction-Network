bebb11fa38b248466ca2b89b7a5800db8e6e4d46
Benjamin Zheng <zhengbenjamin1218@gmail.com>
2025-10-25 16:27:10 -0400

    setup

---
+++ models/LSTM/model.py
@@ -0,0 +1,16 @@
+import torch 
+import torch.nn as nn
+import numpy as np
+
+from typing import Optional, List
+
+class Model(nn.Module):
+
+    def __init__(self):
+        
+        super().__init__()
+        
+    def forward(self, x):
+        pass 
+        
+        
